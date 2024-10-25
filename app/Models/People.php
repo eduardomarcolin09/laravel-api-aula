fe14b4ae00c70aeed495036ce39688ef0d4cb52b
Eduardo <eduardomarcolin09ntj@gmail.com>
2024-10-25 11:33:56 -0300

    Aula do dia 22 e dia 25

---
+++ app/Models/People.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class People extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id', 
+        'name', 
+        'email',
+        'address',
+        'state',
+        'country',
+        'phone',
+        'birthDate'
+    ];
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,35 @@
 <?php
 
+use App\Http\Controllers\PeopleController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'OK'
+    ]);
 });
+
+Route::get('/somar', function(Request $request) {
+
+    // Não está chegando nada pela request
+    if (count($request->all()) < 1) {
+        return response()->json([
+            'message' => 'Não há valores para somar.'
+        ], 406);
+    }
+
+    $soma = array_sum($request->all());
+    return response()->json([
+        'message' => 'OK',
+        'sum' => $soma, 
+    ],200);
+});
+
+Route::prefix('/people')->group(function() {
+    Route::get('/list', [PeopleController::class, 'list']);
+
+    Route::post('/store', [PeopleController::class, 'store']);
+
+});
+
