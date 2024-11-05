1c1f9c6448acb0687294482235257fa811cfeebd
Eduardo <eduardomarcolin09ntj@gmail.com>
2024-11-05 09:03:37 -0300

    aula dia 05/11

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\InterestController;
+use App\Http\Controllers\JWTAuthController;
 use App\Http\Controllers\PeopleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -38,6 +39,9 @@ Route::prefix('/interest')->group(function() {
     Route::get('/list', [InterestController::class, 'list']);
 
     Route::post('/store', [InterestController::class, 'store']);
+});
 
+Route::prefix('/user')->group(function() {
+    Route::post('register', [JWTAuthController::class, 'register']);
 });
 
