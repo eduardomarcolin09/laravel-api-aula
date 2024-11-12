7bba8947b840c5c67c51c8a82869394df9153fe6
Eduardo <eduardomarcolin09ntj@gmail.com>
2024-11-12 08:44:55 -0300

    aula 12/11

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\InterestController;
 use App\Http\Controllers\JWTAuthController;
 use App\Http\Controllers\PeopleController;
+use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -28,7 +29,7 @@ Route::get('/somar', function(Request $request) {
     ],200);
 });
 
-Route::prefix('/people')->group(function() {
+Route::prefix('/people')->middleware([JwtMiddleware::class])->group(function() {
     Route::get('/list', [PeopleController::class, 'list']);
 
     Route::post('/store', [PeopleController::class, 'store']);
@@ -43,5 +44,7 @@ Route::prefix('/interest')->group(function() {
 
 Route::prefix('/user')->group(function() {
     Route::post('register', [JWTAuthController::class, 'register']);
+    Route::post('/login', [JWTAuthController::class, 'login']);
+    Route::middleware([JwtMiddleware::class])->get('/logout', [JWTAuthController::class, 'logout']);
 });
 
